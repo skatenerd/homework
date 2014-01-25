5e6050183227c99d3f4a50cbbb1b4d36d93ba38c
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-01-25 13:50:08 -0600

    Still more nothingness

--- src/homework/core.clj
+++ src/homework/core.clj
@@ -6,7 +6,13 @@
     (first (filter #(find-node predicate %) (:children tree)))))
 
 (defn find-all [predicate get-children tree]
-  #{})
+  (let [new-finds (if (predicate tree)
+                    #{tree}
+                    #{})]
+    (reduce
+      clojure.set/union
+      new-finds
+      (map #(find-all predicate %) (get-children tree)))))
 
 (defn find-node-custom [predicate get-children tree]
   (if (predicate tree)
